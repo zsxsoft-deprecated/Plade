6af01b6c0a3db498a9238a8fd1995fe377e2a48f
zsx <zsx@zsxsoft.com>
2017-05-30 11:28:36 +0800

    [Repo] Remove useless file

--- dist/ipc/showOpenDialog.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const wrapper = require('./wrapper');
-const { dialog } = require('electron');
-module.exports.default = wrapper('showOpenDialog');
-module.exports.main = (options) => new Promise((resolve, reject) => {
-    dialog.showOpenDialog(options, (fileNames) => resolve(fileNames));
-});
-//# sourceMappingURL=showOpenDialog.js.map
